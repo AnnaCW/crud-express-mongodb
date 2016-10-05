06eab11b5eb66c30f7608209b2733631a3377039
Anna Weisbrodt <aweisbro@gmail.com>
2016-10-05 12:10:40 -0600

    add mongo db

--- server.js
+++ server.js
@@ -3,17 +3,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+
+MongoClient.connect('mongodb://acw:expresspwd@ds011963.mlab.com:11963/crud-express-mongodb', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-  console.log('listening on 3000');
-});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body);
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/');
+  });
 });
